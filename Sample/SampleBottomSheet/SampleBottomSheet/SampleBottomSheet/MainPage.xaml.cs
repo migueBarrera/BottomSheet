990d5302f638990d05813f14d2093197a38bfdaf
miguel <mbmdevelop@gmail.com>
2019-11-08 16:59:57 +0100

    small sample

---
+++ Sample/SampleBottomSheet/SampleBottomSheet/SampleBottomSheet/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SampleBottomSheet
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        private MainViewModel ViewModel;
+        public MainPage()
+        {
+            InitializeComponent();
+
+            ViewModel = new MainViewModel();
+
+            BindingContext = ViewModel;
+        }
+
+        protected override void OnSizeAllocated(double width, double height)
+        {
+            base.OnSizeAllocated(width, height);
+            if (SimpleBottomSheet.ParentHeight == 0)
+            {
+                SimpleBottomSheet.ParentHeight = height;
+            }
+        }
+
+        private void SwipeGestureRecognizer_Swiped(object sender, SwipedEventArgs e)
+        {
+            ViewModel.IsOpen = false;
+        }
+    }
+}
---
+++ Sample/SampleBottomSheet/SampleBottomSheet/SampleBottomSheet/MainViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace SampleBottomSheet
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private bool isOpen;
+        public bool IsOpen
+        {
+            get => isOpen;
+            set => SetAndRaisePropertyChanged(ref isOpen, value);
+            }
+
+        public ICommand ClickCommand => new Command(ClickCommandExecute);
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void ClickCommandExecute()
+        {
+            IsOpen = true;
+        }
+
+        protected void SetAndRaisePropertyChanged<TRef>(
+     ref TRef field, TRef value, [CallerMemberName] string propertyName = null)
+        {
+            field = value;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected void SetAndRaisePropertyChangedIfDifferentValues<TRef>(
+            ref TRef field, TRef value, [CallerMemberName] string propertyName = null)
+            where TRef : class
+        {
+            if (field == null || !field.Equals(value))
+            {
+                SetAndRaisePropertyChanged(ref field, value, propertyName);
+            }
+        }
+    }
+}
